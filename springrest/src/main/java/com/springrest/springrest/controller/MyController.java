ec1a627b49ce43b7d4961ce3553088eb33526a7d
jf16321 <nidhi.gupta@kbc.com>
2023-03-20 12:05:06 +0200

    Lab_02_RestApi

--- springrest/src/main/java/com/springrest/springrest/controller/MyController.java
+++ springrest/src/main/java/com/springrest/springrest/controller/MyController.java
@@ -1,41 +1,28 @@
 package com.springrest.springrest.controller;
 
 
-import com.springrest.springrest.services.CocktailsService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import src.main.java.com.springrest.springrest.entities.Cocktails;
 
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
 
 @RestController
 public class MyController {
-
-
-    @Autowired
-    public CocktailsService cocktailsService;
-
-    }
-
-
-    @GetMapping ("/search")
-    public String search()
-
-    {
-        return "This is cocktail";
-    }
-
-
-    //get the list of cocktails
-    @GetMapping("/search")
-    public List<Cocktails> getCocktails()
+    @GetMapping ("/cocktails")
+    public String search(@RequestParam String search)
 
     {
 
-        return this.cocktailsService.getCocktails();
 
+
+        return "this is Cocktail API";
     }
+
+
+
 }
 
 
