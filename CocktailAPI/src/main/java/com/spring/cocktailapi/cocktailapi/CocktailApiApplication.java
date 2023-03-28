80b9b32098a4e427e01787273aa819b99bec4b86
jf16321 <nidhi.gupta@kbc.com>
2023-03-28 14:32:44 +0300

    Ni_Lab_02_RestApi

---
+++ CocktailAPI/src/main/java/com/spring/cocktailapi/cocktailapi/CocktailApiApplication.java
@@ -0,0 +1,13 @@
+package com.spring.cocktailapi.cocktailapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocktailApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CocktailApiApplication.class, args);
+    }
+
+}
---
+++ CocktailAPI/src/test/java/com/spring/cocktailapi/cocktailapi/CocktailApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.cocktailapi.cocktailapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocktailApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
